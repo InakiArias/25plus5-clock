7e66a45f195ec84b23add7f0b352304e9dac8a78
InakiArias <87582631+InakiArias@users.noreply.github.com>
2022-04-14 21:18:00 -0300

    deleted items

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import DrumPad from './components/DrumPad';
-import BUTTONS_DATA from './constants/BUTTONS_DATA';
-import Display from './components/Display';
-import playAudio from './utils/playAudio';
-import setFocus from './utils/setFocus';
 
 function App() {
-  const [playingPadName, setPlayingPadName] = useState("-----");
-  useEffect(setFocus, []);
-
-  const onClick = (letter, id) => {
-    playAudio(letter);
-    setPlayingPadName((prevName) => id);
-  }
-
-  const onKeyDown = (event) => {
-    let letter = event.key.toUpperCase();
-
-    let name = BUTTONS_DATA.find(button => button.buttonLetter === letter).soundName;
-
-    setPlayingPadName((prevName) => name);
-
-    playAudio(letter);
-  }
-
-  const onEnded = (event) => {
-    let letter = event.target.id;
-    let name = BUTTONS_DATA.find(button => button.buttonLetter === letter).soundName;
-
-    setPlayingPadName((prevName) => 
-      name === prevName ? "-----" : prevName
-    );
-  }
+  
 
   return (
-    <div 
-        id="drum-machine"
-        tabIndex={0}
-        onBlur={(e) => setFocus()}
-        onKeyDown={onKeyDown}
-        onClick={(e) => setFocus()}
-    >
-      {BUTTONS_DATA.map((button) =>
-        <DrumPad
-          key={button.buttonLetter}
-          letter={button.buttonLetter}
-          id={button.soundName}
-          url={button.soundUrl}
-          onClick={onClick}
-          onEnded={onEnded}
-          playingPadName={playingPadName}
-        />)}
-      <Display playingPadName={playingPadName} />
+    <div>
+       
     </div>
   );
 }
--- src/components/Display.js
+++
@@ -1,5 +0,0 @@
-import './Display.css';
-
-const Display = (props) => <div id="display">{props.playingPadName}</div>;
-
-export default Display;
